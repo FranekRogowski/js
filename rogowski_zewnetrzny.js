aab3cb0a1280bf57a0e381f9d9008f443beb74c4
FranekRogowski <franekrogowski4@gmail.com>
2025-06-11 10:44:28 +0200

    Update rogowski_zewnetrzny.js

--- rogowski_zewnetrzny.js
+++ rogowski_zewnetrzny.js
@@ -1,2 +1,2 @@
-document.write("ostatnia modyfikacja strony".fontcolor("blue").bold().strike().fontsize(5)+"<br>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("red").strike().fontsize(6)+"<br>"); 
 document.write(document.lastModified); 
